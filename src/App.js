97c57be6c0015f6c6a6c6062fd972e1822f395fb
NilsRondhuis <nilsrondhuis@ukr.net>
2022-04-05 14:13:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import ProductList from "./components/ProductList";
+import Section from "./components/Section";
+import products from "./product.json";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Section title="Top this week">
+        <ProductList items={products} />
+      </Section>
+    </>
   );
 }
-
-export default App;
---
+++ src/components/Product.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import defaultImg from "../images/default.jpg";
+
+export default function Product({
+  id,
+  img = defaultImg,
+  name,
+  desc,
+  price,
+  quantity,
+}) {
+  return (
+    <>
+      <p>ID: {id}</p>
+      <img src={img} alt={name} />
+      <h2>{name}</h2>
+      <p>{desc}</p>
+      <p>Price: {price}</p>
+      <p>Quantity: {quantity < 10 ? "min" : "max"}</p>
+    </>
+  );
+}
+
+Product.propTypes = {
+  id: PropTypes.string,
+  img: PropTypes.string,
+  name: PropTypes.string,
+  desc: PropTypes.string,
+  price: PropTypes.number,
+  quantity: PropTypes.number,
+};
